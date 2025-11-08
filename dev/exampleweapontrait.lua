6b10d6acedf039aa255b735a0aa926c6f6b2f3ba
zanncdwbl <xspelz@gmail.com>
2025-11-08 00:31:08 +0000

    docs

--- dev/exampleweapontrait.lua
+++ dev/exampleweapontrait.lua
@@ -7,7 +7,7 @@ game.TraitData.ArtemisWeaponBoon = {
 	Name = "ArtemisWeaponBoon",
 	BoonInfoTitle = "ArtemisWeaponBoon",
 	Icon = "Boon_Artemis_ArtemisWeaponBoon",
-	Slot = "Melee", -- If not a core slot, omit this.
+	Slot = "Melee",
 	Cost = 30,
 	BlockStacking = false,
 	RarityLevels = {
@@ -55,18 +55,9 @@ game.TraitData.ArtemisWeaponBoon = {
 	} },
 }
 
--- Insert into default game Artemis, and New Artemis Drop
-table.insert(game.EnemyData.NPC_Artemis_Field_01.Traits, "ArtemisWeaponBoon")
-
 -- Omit if you are not adding any core boons, for example: ( "ApolloWeaponBoon", "ApolloSpecialBoon", "ApolloCastBoon", "ApolloSprintBoon", "ApolloManaBoon" )
 -- This is to allow for the game to check if the trait needs to be replaced (AKA Core slots, Melee, Special, if your trait does not add to core slots, this s not needed.)
+table.insert(game.EnemyData.NPC_Artemis_Field_01.WeaponUpgrades, 1, "ArtemisWeaponBoon")
 table.insert(game.EnemyData.NPC_Artemis_Field_01.PriorityUpgrades, 1, "ArtemisWeaponBoon")
-
--- You do not need to insert the new boon into LootData traits as Droppable Gods already does that automatically.
-table.insert(game.LootData.ArtemisUpgrade.WeaponUpgrades, 1, "ArtemisWeaponBoon") -- If you are not adding Core Boons, this can also be ommited.
-table.insert(game.LootData.ArtemisUpgrade.PriorityUpgrades, 1, "ArtemisWeaponBoon") -- ? Same as previous priority.
-
-table.insert(game.LinkedTraitData.WeaponTraits, "ArtemisWeaponBoon") -- Omit if you are not adding anything referring to Weapon
-table.insert(game.LinkedTraitData.ArtemisCoreTraits, "ArtemisWeaponBoon") -- Omit if you are not adding any core boons, for example: ( "ApolloWeaponBoon", "ApolloSpecialBoon", "ApolloCastBoon", "ApolloSprintBoon", "ApolloManaBoon" )
 
 game.TraitData.ArtemisWeaponBoon.TraitOrderingValueCache = GetTraitOrderingValue(game.TraitData.ArtemisWeaponBoon) -- Get the Order value automatically.
